40500faa910f6f33d922dc66ae4d11fcf57125c1
bethbenjamin <bethbenjamin@gmail.com>
2018-01-28 17:54:12 -0500

    trying to get the matches to add and repeat game, but everything works from there

--- LuckyNumbers/Program.cs
+++ LuckyNumbers/Program.cs
@@ -17,9 +17,13 @@ namespace LuckyNumbers
             string lowNumberMessage = "Please choose a low number";
             string welcomeMessage = "Welcome to Lucky Numbers! The jackpot is $600,000.00.  Good Luck!";
             int highNumber, lowNumber;
+            bool playAgain = true;
+            bool quit = false;
+            bool isEqual;
+            int matches = 0;
+
+
             
-
-
             Console.WriteLine(welcomeMessage); //welcome message
             Console.WriteLine(highNumberMessage); //enter high #
             highNumber = int.Parse(Console.ReadLine());//turn ask into number variable
@@ -27,22 +31,24 @@ namespace LuckyNumbers
             lowNumber = int.Parse(Console.ReadLine()); // turn ask into number variable
 
 
+
             //ask user to guess 6 lucky numbers in an array data structure and must use a loop to populate the array
             int g; //g = guess (in my mind) to reference it.
             int[] luckyGuess = new int[6]; // 6 size array
             Console.WriteLine("Please enter 6 Lucky Numbers.");
             // Accepting input value from user 
-            for (g = 0; g < 6; g++)
+            
+                for (g = 0; g < 6; g++)
             {
                 Console.Write("Enter your lucky number:");
 
-                luckyGuess [g] = Convert.ToInt32(Console.ReadLine()); //storing value in the array
+                luckyGuess[g] = Convert.ToInt32(Console.ReadLine()); //storing value in the array
             }
             Console.WriteLine("\n"); //carrage return to display their value // found this on microsoft community forum /i think we discussed it in class though.
             //we are going to be printing the value on console
             for (g = 0; g < 6; g++)
             {
-                Console.WriteLine("You've selected : {0}", luckyGuess [g]); //telling user their lucky number output
+                Console.WriteLine("You've selected : {0}", luckyGuess[g]); //telling user their lucky number output
             }
             Console.ReadLine();
 
@@ -55,15 +61,41 @@ namespace LuckyNumbers
                 Console.WriteLine("Lucky Number: " + rArray[i]); //display numbers per line with the word "Lucky Number : x <br>
             }
 
-            // create for loop to compare the input numbers and the random numbers // possibly comparing two arrays?
-            // if guess numbers match, add x amount to each matching number
-            // if numbers match display match amount
 
-            
-             
-          
 
-            
+            for (int k = 0; k < luckyGuess.Length; k++) //iterates through the lucky guess
+            {
+                if (rArray.Contains(luckyGuess[k])) //if random array contains value of element at index of k add one
+                {
+                    matches += 1; //if rarray finds a match it increments one to matches
+                    Console.WriteLine("You guessed " + matches + "numbers correctly!");
+                }
+
+            }
+            if (matches == 1)
+                Console.WriteLine("You matched 1, you won" +matches * 100,000);
+            else if (matches == 2)
+                Console.WriteLine("You matched 2, you won");
+            else if (matches == 3)
+                Console.WriteLine("You matched 3, you won");
+            else if (matches == 4)
+                Console.WriteLine("You matched 4, you won");
+            else if (matches == 5)
+                Console.WriteLine("You matched 5, you won");
+            else if (matches == 6)
+                Console.WriteLine("You won the jackpot");
+            else if (matches == 0)
+                Console.WriteLine("Sorry, you didnt match any");
+
+           
+
+                Console.WriteLine("Try again? Type exit to quit");
+                
         }
+
+
+
+
+
     }
 }
